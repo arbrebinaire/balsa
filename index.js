3e433d134d85f2b69f84ea7554ea2c836d791732
François Hétu <francois@arbrebinaire.com>
2020-02-21 11:08:16 -0500

    Auto-updater: corrections

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const figlet = require('figlet');
 
 const install = require('./lib/installer').installBalsa;
 const run = require('./lib/runner').run;
-const update = require('./lib/update').update;
+const update = require('./lib/updater').update;
 
 
 clear();
