ec28ae940fb0f63deca35e4c2dfac530cb827107
François Hétu <francois@arbrebinaire.com>
2020-02-21 11:05:11 -0500

    Auto-updater

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const figlet = require('figlet');
 
 const install = require('./lib/installer').installBalsa;
 const run = require('./lib/runner').run;
+const run = require('./lib/update').run;
 
 
 clear();
@@ -20,17 +21,24 @@ var argv = require('minimist')(process.argv.slice(2));
 
 switch (argv._[0]) {
     case "install":
-        if(argv._.length == 1){
+        if (argv._.length === 1) {
             install()
         } else {
             console.log(`Il n'est pas encore possible de lancer l'installation de [${argv._[1]}]`)
         }
         break;
     case "run":
-        if(argv._.length > 1){
+        if (argv._.length > 1) {
             run(argv._[1])
         } else {
             console.log("Il faut au moins le nom du programme à lancer: balsa run [nom_du_programme]")
+        }
+        break;
+    case "update":
+        if (argv._.length === 1) {
+            update()
+        } else {
+            console.log(`Il n'est pas encore possible de lancer la mise à jour de [${argv._[1]}]`)
         }
         break;
     default:
