7353a413e3a08ca5cdd3ea3deedf1102eceb4872
François Hétu <francois@arbrebinaire.com>
2020-03-16 21:14:40 -0400

    Mise en forme des messages

--- renew_balsa.sh
+++ renew_balsa.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 BALSADIR="$HOME"/.config/balsa/balsa
-#BALSADIR="$HOME"/Desktop/.config/balsa/balsa
-#mkdir -p "$BALSADIR"
-#touch "$HOME"/Desktop/.config/balsa/balsa/somefile.txt
 
 cd "$BALSADIR" 2>&1 && {
     cd .. 2>&1 && \
@@ -12,13 +9,16 @@ cd "$BALSADIR" 2>&1 && {
     cd balsa 2>&1 && \
     git clone https://github.com/arbrebinaire/balsa.git . 2>&1 && \
     npm install -g 2>&1 && {
+        echo
         echo "Succès: Balsa a été réinstallé de tout frais :-)"
         exit 0
     } || {
+        echo
         echo "Échec de la réinstallation de Balsa: envoyer le fichier $(pwd)/renew_balsa_log.txt au diable de programmeur"
         exit 1
     }
 } || {
+    echo
     echo "Impossible de changer le répertoire actif pour ["$HOME"/.config/balsa/balsa].  Échec de la réinstallation de Balsa"
     exit 1
 }
